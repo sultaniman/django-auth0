06394a9f46da3ef3c21da159e436f8cb9cc845d4
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-04-30 20:01:41 +0600

    update(requirements) add requests to requirements

--- setup.py
+++ setup.py
@@ -14,10 +14,12 @@ except ImportError:
 def get_version(*file_paths):
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]',
                               version_file, re.M)
+
     if version_match:
         return version_match.group(1)
+
     raise RuntimeError('Unable to find version string.')
 
 
@@ -27,7 +29,7 @@ if sys.argv[-1] == 'publish':
     try:
         import wheel
     except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
+        print('Wheel library missing. Please run pip install wheel')
         sys.exit()
 
     os.system('python setup.py sdist upload')
@@ -35,9 +37,10 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 if sys.argv[-1] == 'tag':
-    print("Tagging the version on github:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
+    print('Tagging the version on github:')
+
+    os.system('git tag -a %s -m \'version %s\'' % (version, version))
+    os.system('git push --tags')
     sys.exit()
 
 readme = open('README.rst').read()
@@ -46,7 +49,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 setup(
     name='django-auth0',
     version=version,
-    description="""Django Auth0 authentication background""",
+    description='''Django Auth0 authentication background''',
     long_description=readme + '\n\n' + history,
     author='Sultan Imanhodjaev',
     author_email='sultan.imanhodjaev@gmail.com',
@@ -54,7 +57,8 @@ setup(
     packages=['django_auth0'],
     include_package_data=True,
     install_requires=[
-        'Django>=1.8'
+        'Django>=1.8',
+        'requests >= 2.10.0'
     ],
     license='MIT',
     zip_safe=False,
