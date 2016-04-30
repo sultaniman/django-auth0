999612ea6118e8a23dbf8dc23bcb8511f7dcdb57
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-04-30 22:53:19 +0600

    add(tests) add auth backend tests

--- tests/test_auth_helper.py
+++ tests/test_auth_helper.py
@@ -4,20 +4,31 @@
 """
 test_django-auth0
 ------------
-
 Tests for `django-auth0` models module.
 """
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django_auth0.auth_backend import Auth0Backend
 
 
-class TestDjango_auth0(TestCase):
-
+class TestDjangoAuth0(TestCase):
     def setUp(self):
-        pass
+        self.backend = Auth0Backend()
+        self.auth_data = {
+            'email': 'email@email.com',
+            'username': 'test_username'
+        }
 
-    def test_something(self):
-        pass
+    def test_authenticate_works(self):
+        """ Authenticate works ok """
+        user = self.backend.authenticate(**self.auth_data)
+        self.assertTrue(isinstance(user, User))
 
-    def tearDown(self):
-        pass
+    def test_authenticate_creates_user(self):
+        """ Authenticate works creates a user """
+        user = self.backend.authenticate(**self.auth_data)
+        self.assertIsNotNone(user)
+
+    def test_authenticate_fires_exception(self):
+        """ Authenticate fires exception when insufficient data supplied """
+        self.assertRaises(ValueError, self.backend.authenticate)
