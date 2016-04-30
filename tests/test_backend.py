253a86d9fc7ab4306993690d10ad64f15fb02af3
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-04-30 22:54:17 +0600

    chore(minor) minor changes

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -8,13 +8,12 @@ test_django-auth0
 Tests for `django-auth0` models module.
 """
 
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
 
 class TestDjango_auth0(TestCase):
-
     def setUp(self):
-        pass
+        self.factory = RequestFactory()
 
     def test_something(self):
         pass
