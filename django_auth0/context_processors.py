8a427cc0fb877769f18bad1397db215e0e2e3b18
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-04-30 20:49:54 +0600

    add(AUTH0_CALLBACK_URL) update context processor

--- django_auth0/context_processors.py
+++ django_auth0/context_processors.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
 
 
@@ -5,5 +6,6 @@ def auth0(request):
     return {
         'AUTH0_CLIENT_ID': settings.AUTH0_CLIENT_ID,
         'AUTH0_SECRET': settings.AUTH0_SECRET,
-        'AUTH0_DOMAIN': settings.AUTH0_DOMAIN
+        'AUTH0_DOMAIN': settings.AUTH0_DOMAIN,
+        'AUTH0_CALLBACK_URL': settings.AUTH0_CALLBACK_URL
     }
