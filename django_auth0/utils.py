3f41969da9775691e5699a1d95feb4097fa5735d
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-05-08 18:34:10 +0600

    add(AUTH0_SUCCESS_URL) to config

--- django_auth0/utils.py
+++ django_auth0/utils.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from .settings import AUTH0_CALLBACK_URL
 
 
 def get_config():
@@ -9,5 +8,6 @@ def get_config():
         'AUTH0_CLIENT_ID': settings.AUTH0_CLIENT_ID,
         'AUTH0_SECRET': settings.AUTH0_SECRET,
         'AUTH0_DOMAIN': settings.AUTH0_DOMAIN,
-        'AUTH0_CALLBACK_URL': AUTH0_CALLBACK_URL
+        'AUTH0_CALLBACK_URL': settings.AUTH0_CALLBACK_URL,
+        'AUTH0_SUCCESS_URL': settings.AUTH0_SUCCESS_URL,
     }
