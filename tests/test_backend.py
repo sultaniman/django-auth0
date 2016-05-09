82cfd60df40f36a15dc26d3b0ed8814280c21af9
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-05-10 00:08:34 +0600

    update(test)

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -5,14 +5,29 @@
 Tests for `django-auth0` authentication backend.
 """
 from django.test import TestCase
-from django_auth0.context_processors import auth0
+from django.contrib.auth.models import User
+from django_auth0.auth_backend import Auth0Backend
 
 
-class TestDjangoAuth0ContextProcessor(TestCase):
-    """ Test Auth0Backend ContextProcessor """
+def _get_user(backend):
+    return backend.get_user(-1)
 
-    def test_context_processor(self):
-        """ It returns dict with settings """
-        config = auth0(None)
-        self.assertTrue(len(config.keys()) > 0)
-        self.assertTrue(type(config) == dict)
+
+class TestDjangoAuth0(TestCase):
+    """ Test Auth0Backend """
+
+    def setUp(self):
+        self.backend = Auth0Backend()
+        self.user = User.objects.create_user(username='user',
+                                             email='email@email.com')
+
+    def test_existing_user_returned(self):
+        """ Test if get user returns a user if it exists in database """
+        self.assertIsNotNone(self.backend.get_user(self.user.pk))
+
+    def test_exception_raised_when_user_not_found(self):
+        """ Test if get user returns a user if it exists in database """
+        self.assertRaises(User.DoesNotExist, self._get_user)
+
+    def _get_user(self):
+        return _get_user(self.backend)
--- tests/test_context_processor.py
+++ tests/test_context_processor.py
@@ -5,29 +5,14 @@
 Tests for `django-auth0` authentication backend.
 """
 from django.test import TestCase
-from django.contrib.auth.models import User
-from django_auth0.auth_backend import Auth0Backend
+from django_auth0.context_processors import auth0
 
 
-def _get_user(backend):
-    return backend.get_user(-1)
+class TestDjangoAuth0ContextProcessor(TestCase):
+    """ Test Auth0Backend ContextProcessor """
 
-
-class TestDjangoAuth0(TestCase):
-    """ Test Auth0Backend """
-
-    def setUp(self):
-        self.backend = Auth0Backend()
-        self.user = User.objects.create_user(username='user',
-                                             email='email@email.com')
-
-    def test_existing_user_returned(self):
-        """ Test if get user returns a user if it exists in database """
-        self.assertIsNotNone(self.backend.get_user(self.user.pk))
-
-    def test_exception_raised_when_user_not_found(self):
-        """ Test if get user returns a user if it exists in database """
-        self.assertRaises(User.DoesNotExist, self._get_user)
-
-    def _get_user(self):
-        return _get_user(self.backend)
+    def test_context_processor(self):
+        """ It returns dict with settings """
+        config = auth0(None)
+        self.assertTrue(len(config.keys()) > 0)
+        self.assertTrue(type(config) == dict)
