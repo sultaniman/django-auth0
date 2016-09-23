7b46b87c8b82571eb94f70f25d3f27861437ba52
Brendon John Muschamp <brendon@numerical.co.nz>
2016-09-23 13:27:10 +1200

    merge fix-UserCreation with fix-AuthFallback on master

--- django_auth0/auth_backend.py
+++ django_auth0/auth_backend.py
@@ -22,7 +22,6 @@ class Auth0Backend(object):
         :param kwargs: user information provided by auth0
         :return: user
         """
-
         is_auth0 = True
 
         # check that each auth0 key is present in kwargs
@@ -35,18 +34,22 @@ class Auth0Backend(object):
         if is_auth0 is False:
             return None
 
-        email = kwargs.pop('email')
-        username = kwargs.pop('nickname')
+        user_id = kwargs.get('user_id')
 
-        if username and email:
-            try:
-                return UserModel.objects.get(email__iexact=email,
-                                             username__iexact=username)
-            except UserModel.DoesNotExist:
-                return UserModel.objects.create(email=email,
-                                                username=username)
+        if user_id is None:
+            raise ValueError(_('user_id can\'t be blank!'))
 
-        raise ValueError(_('Username or email can\'t be blank'))
+        # The format of user_id is {identity provider id}|{unique id in the provider}
+        # The pipe character is invalid for the django username field
+        # The solution is to replace the pipe with a dash
+        username = user_id.replace('|', '-')
+
+        try:
+            user = UserModel.objects.get(username__iexact=username)
+        except UserModel.DoesNotExist:
+            user = UserModel.objects.create(username=username)
+
+        return user
 
     # noinspection PyProtectedMember
     def get_user(self, user_id):
