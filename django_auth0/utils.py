02821c003fe85bbafad14eaac40dbaed295ddbbd
Peter Novotnak <peter@cyrusbio.com>
2017-05-09 16:51:38 -0700

    working backwards from stormpath models

--- django_auth0/utils.py
+++ django_auth0/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from auth0.v3.authentication import GetToken
 from django.conf import settings
 
 
@@ -11,3 +12,23 @@ def get_config():
         'AUTH0_CALLBACK_URL': settings.AUTH0_CALLBACK_URL,
         'AUTH0_SUCCESS_URL': settings.AUTH0_SUCCESS_URL,
     }
+
+
+def get_token(*, a0_config: dict=None, audience=None) -> str:
+    """ 
+    Generate an authentication connection to Auth0 service
+
+    :param a0_config: Auth0 configuration parameters
+    :param audience: Token audience (defaults to f'https://{domain}/api/v2/') 
+    """
+
+    if not audience:
+        audience = 'https://%s/api/v2/' % a0_config["AUTH0_DOMAIN"]
+
+    r = GetToken(a0_config['AUTH0_DOMAIN'])
+    token = r.client_credentials(
+        a0_config['AUTH0_CLIENT_ID'],
+        a0_config['AUTH0_SECRET'],
+        audience
+    )
+    return token['access_token']
